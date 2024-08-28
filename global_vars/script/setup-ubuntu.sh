5d8a63d0c18be4b1abd21a112a294bd23f87988d
BRAVO\tuanbd <bachdangtuan.dev@gmail.com>
2024-08-28 11:27:32 +0700

    update

--- global_vars/script/setup-ubuntu.sh
+++ global_vars/script/setup-ubuntu.sh
@@ -37,3 +37,8 @@ systemctl restart containerd
 
 curl -fsSL https://pkgs.k8s.io/core:/stable:/$VERSION/deb/Release.key | sudo gpg --dearmor --batch --yes -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$VERSION/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+#############
+apt-get update
+sudo apt-get install -y kubeadm=$VERSION_k8s.$VERSION_PATCH kubelet=$VERSION_k8s.$VERSION_PATCH kubectl=$VERSION_k8s.$VERSION_PATCH --allow-change-held-packages
+apt-mark hold kubelet kubeadm kubectl
