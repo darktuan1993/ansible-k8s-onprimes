d1ecbcfcbb26db2edb3911d08a704c7f2968633e
BRAVO\tuanbd <bachdangtuan.dev@gmail.com>
2024-08-28 09:52:42 +0700

    update

--- global_vars/script/sysctl.sh
+++ global_vars/script/sysctl.sh
@@ -3,3 +3,7 @@ net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
 EOF
+
+sysctl --system
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
