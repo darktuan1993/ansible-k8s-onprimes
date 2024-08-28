68230a5593e7544636b03a7ee5b593a94defc6ea
BRAVO\tuanbd <bachdangtuan.dev@gmail.com>
2024-08-28 11:35:45 +0700

    update

--- global_vars/script/delete-ubuntu.sh
+++ global_vars/script/delete-ubuntu.sh
@@ -1,21 +1,21 @@
 unset KUBECONFIG
-sudo kubeadm reset
-sudo rm -rf /etc/cni/net.d
-sudo rm -rf /var/lib/cni/
-sudo rm -rf /var/run/kubernetes/
-sudo docker system prune -af
-sudo systemctl stop docker
-sudo apt-get remove --purge docker-ce docker-ce-cli containerd.io
-sudo systemctl stop containerd
-sudo apt-get remove --purge containerd
-sudo apt-get remove --purge kubeadm kubectl kubelet kubernetes-cni kube*
-sudo rm -rf ~/.kube
-sudo rm -rf /etc/kubernetes/
-sudo rm -rf /var/lib/etcd/
-sudo rm -rf /var/lib/kubelet/
-sudo rm -rf /var/lib/dockershim
-sudo rm -rf /etc/systemd/system/kubelet.service.d
-sudo rm -rf /etc/systemd/system/kubelet.service
-sudo rm -rf /usr/bin/kube*
-sudo rm -rf /usr/local/bin/kube*
-sudo reboot
+kubeadm reset
+rm -rf /etc/cni/net.d
+rm -rf /var/lib/cni/
+rm -rf /var/run/kubernetes/
+docker system prune -af
+systemctl stop docker
+apt-get remove --purge docker-ce docker-ce-cli containerd.io
+systemctl stop containerd
+apt-get remove --purge containerd
+apt-get remove --purge kubeadm kubectl kubelet kubernetes-cni kube*
+rm -rf ~/.kube
+rm -rf /etc/kubernetes/
+rm -rf /var/lib/etcd/
+rm -rf /var/lib/kubelet/
+rm -rf /var/lib/dockershim
+rm -rf /etc/systemd/system/kubelet.service.d
+rm -rf /etc/systemd/system/kubelet.service
+rm -rf /usr/bin/kube*
+rm -rf /usr/local/bin/kube*
+reboot
