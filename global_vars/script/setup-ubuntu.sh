650d98e0d88362f1573f5fa9f728edd404db047f
BRAVO\tuanbd <bachdangtuan.dev@gmail.com>
2024-08-28 11:19:51 +0700

    update

--- global_vars/script/setup-ubuntu.sh
+++ global_vars/script/setup-ubuntu.sh
@@ -20,3 +20,6 @@ net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
 EOF
 }
+
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor --batch --yes -o /etc/apt/keyrings/docker.gpg
