27283f81dae651d9213428e3cdca8c9972aa68b8
BRAVO\tuanbd <bachdangtuan.dev@gmail.com>
2024-08-28 11:29:04 +0700

    update

--- global_vars/script/setup-ubuntu.sh
+++ global_vars/script/setup-ubuntu.sh
@@ -42,3 +42,23 @@ echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.
 apt-get update
 sudo apt-get install -y kubeadm=$VERSION_k8s.$VERSION_PATCH kubelet=$VERSION_k8s.$VERSION_PATCH kubectl=$VERSION_k8s.$VERSION_PATCH --allow-change-held-packages
 apt-mark hold kubelet kubeadm kubectl
+
+
+
+############
+
+wget https://github.com/kubernetes-sigs/cri-tools/releases/download/$VERSION.0/crictl-$VERSION.0-linux-amd64.tar.gz
+
+tar zxvf crictl-$VERSION.0-linux-amd64.tar.gz
+
+mv crictl /usr/local/bin
+
+cat <<EOF | tee /etc/crictl.yaml
+runtime-endpoint: unix:///run/containerd/containerd.sock
+EOF
+
+sudo wget https://storage.googleapis.com/gvisor/releases/nightly/latest/containerd-shim-runsc-v1 -O /usr/local/bin/containerd-shim-runsc-v1
+sudo chmod +x /usr/local/bin/containerd-shim-runsc-v1
+
+sudo wget https://storage.googleapis.com/gvisor/releases/nightly/latest/runsc -O /usr/local/bin/runsc
+sudo chmod +x /usr/local/bin/runsc
