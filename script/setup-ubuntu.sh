5e77d5a5a71d7620986fb94861b9957b42d47999
DESKTOP-TG45HMC\DarkTuan <bachdangtuan.dev@gmail.com>
2024-08-31 01:49:21 +0700

    no message

--- script/setup-ubuntu.sh
+++ script/setup-ubuntu.sh
@@ -16,13 +16,13 @@ echo $VERSION_PATCH
 
 
 
-{
+
 cat << EOF | tee /etc/sysctl.d/kubernetes.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
 EOF
-}
+
 
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor --batch --yes -o /etc/apt/keyrings/docker.gpg
