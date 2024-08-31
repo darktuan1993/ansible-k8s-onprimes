b47adabb405fd3c56294d34bfcd16b0d11efdc43
DESKTOP-TG45HMC\DarkTuan <bachdangtuan.dev@gmail.com>
2024-08-31 07:10:08 +0700

    no message

--- script/setup-ubuntu.sh
+++ script/setup-ubuntu.sh
@@ -1,11 +1,11 @@
-apt-get update && apt-get upgrade -y
-apt-get install -y vim
-apt install curl apt-transport-https vim git nfs-common wget software-properties-common lsb-release ca-certificates -y
-swapoff -a; sed -i '/swap/d' /etc/fstab
-modprobe overlay
-modprobe br_netfilter
-echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
-echo 'br_netfilter' > /etc/modules-load.d/k8s.conf
+# apt-get update && apt-get upgrade -y
+# apt-get install -y vim
+# apt install curl apt-transport-https vim git nfs-common wget software-properties-common lsb-release ca-certificates -y
+# swapoff -a; sed -i '/swap/d' /etc/fstab
+# modprobe overlay
+# modprobe br_netfilter
+# echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
+# echo 'br_netfilter' > /etc/modules-load.d/k8s.conf
 
 
 VERSION_k8s=$1
@@ -17,12 +17,12 @@ echo $VERSION_PATCH
 
 
 
-cat << EOF | tee /etc/sysctl.d/kubernetes.conf
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-net.ipv4.ip_forward = 1
-EOF
-sysctl -p
+# cat << EOF | tee /etc/sysctl.d/kubernetes.conf
+# net.bridge.bridge-nf-call-ip6tables = 1
+# net.bridge.bridge-nf-call-iptables = 1
+# net.ipv4.ip_forward = 1
+# EOF
+# sysctl -p
 
 
 mkdir -p /etc/apt/keyrings
